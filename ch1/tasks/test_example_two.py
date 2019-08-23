451627e45213c353e93a9c0ac229f00a4ffb98ce
Albert Fougy <albert@fougy.com>
2019-08-23 11:32:02 -0400

    delete  Please enter the commit message for your changes. Lines starting

--- ch1/tasks/test_example_two.py
+++
@@ -1,6 +0,0 @@
-import pytest 
-
-def test_a_number():
-    a_list = [3,4,5,6,7]
-    a_number = [x for x in a_list if x == a_list[1]]
-    assert [4]== a_number 
--- ch1/tasks/text_example_two.py
+++
@@ -1,8 +0,0 @@
-import pytest 
-
-
-@pytest.mark.mark_this_my_gee
-def test_a_number():
-    a_list = [3,4,5,6,7]
-    a_number = [x for x in a_list if x == 4 ]
-    assert a_number == 4
